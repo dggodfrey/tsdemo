e20250d1e857166d87bad50f7ae8b49c991b3de4
David Godfrey <dgodfrey@bamboohr.com>
2020-05-04 16:31:03 -0600

    Step 3

--- src/utils/ajax.ts
+++ src/utils/ajax.ts
@@ -1,8 +1,14 @@
-function get() {
+export interface userData {
+    name: string;
+    id: string;
+    displayName: string
+}
+
+export function get(): Promise<userData> {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             // some asynchronous request to BE
-            const data = {
+            const data: userData = {
                 name: 'david',
                 id: '007',
                 displayName: 'Digger'
